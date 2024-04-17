54e9c0b9484f77320f230bc16a0049d909a30190
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-04-17 12:51:39 +0330

    complete navbar

--- project-1/sass/js/app.js
+++ project-1/sass/js/app.js
@@ -1,7 +1,47 @@
 const navMenu = document.getElementById("nav-menu");
 const navToggle = document.getElementById("nav-toggle");
+const navLink = document.querySelectorAll(".nav__link")
+
 
 // show menu
 navToggle.addEventListener("click", () => {
   navMenu.classList.toggle("show-menu");
 });
+
+// changebackground header
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  if (this.scrollY >= 80) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
+
+// remove menu
+function linkAction() {
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+// scroll section active link
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 50,
+      sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(`.nav__menu a[href*= ${sectionId}]`)
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(`.nav__menu a[href*= ${sectionId}]`)
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
